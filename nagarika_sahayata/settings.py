b09d71a0b720d18402c03e13de506cd0b885a3a2
unknown <rohitmadame2010@gmail.com>
2025-03-06 14:45:56 +0530

    Intial commit

--- nagarika_sahayata/settings.py
+++ nagarika_sahayata/settings.py
@@ -11,10 +11,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'vmw8YTk9SNOS4iXosN8xqM0A3dvs6Lea_3r5Er42x-5uddYjDZavI6KBL1rgIgM9lDc'
-DEBUG = 'False'
+DEBUG = 'True'
 # SECURITY WARNING: don't run with debug turned on in production!
 
-ALLOWED_HOSTS = ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
@@ -64,16 +64,13 @@ WSGI_APPLICATION = 'nagarika_sahayata.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 load_dotenv()
 
-
 DATABASES = {
     'default': {
-        'ENGINE': 'djongo',
-        'NAME': os.getenv('nagarik_db'),
-        'CLIENT': {
-            'host': os.getenv('mongodb://mongo:nltfjEjShgxNYbHyoSBTFFEUPWSVAQju@mongodb.railway.internal:27017'),
-        }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
