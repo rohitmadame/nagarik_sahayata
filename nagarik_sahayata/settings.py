26ffe39090897d7afcc33ad4bed6f21adf495820
unknown <rohitmadame2010@gmail.com>
2025-03-07 17:14:57 +0530

    created staticfiles folder

--- nagarik_sahayata/settings.py
+++ nagarik_sahayata/settings.py
@@ -60,10 +60,7 @@ WSGI_APPLICATION = 'nagarik_sahayata.wsgi.application'
 
 # Database Configuration
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default=os.getenv('postgresql://postgres:ptAaMrQoCUSqEdozuGUuXbulitMcBncM@postgres.railway.internal:5432/railway'))
 }
 
 # Password validation
