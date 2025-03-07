94dc2b57df716d7722fe6dcae78aa0958d09d8e0
unknown <rohitmadame2010@gmail.com>
2025-03-07 12:36:03 +0530

    updated wsgi

--- nagarika_sahayata/wsgi.py
+++ nagarika_sahayata/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nagarika_sahayata.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nagarik_sahayata.settings')
 
 application = get_wsgi_application()
